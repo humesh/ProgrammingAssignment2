33700e37394bde57cf6d4bacc2a0aa8658704382
humeshkar <humeshkar@gmail.com>
2014-04-27 22:59:33 -0400

    Better Shape

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,23 @@
 ## The following two functions deal with Matrix manipulation in R,
 ## especially in the context of caching matrices.
+##We assume that x is a square invertible matrix
 
-## This function takes a matrix and returns its inverse in the cache
+## This function takes a matrix and creates a special "matrix" object that caches
+##its inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
+  Mx<-NULL
+  set<-function(y){
+    x<<-y
+    Mx<<-NULL
+  }
+  get<-function() x
+  setInverse<-function(Invx) Mx<<-Invx
+  getInverse<-function() Mx
+  finallist<-list(set=set, get=get,
+       setInverse=setInverse,
+       getInverse=getInverse)
+  return(finallist)
 
 }
 
@@ -13,6 +27,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## has already been calculated(and the matrix has not changed) then the 
 ## cacheSolve retrieves the inverse from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(y) {
+  ## Return a matrix that is the inverse of 'y'
+  Invy<-y$getInverse()
+  If(!is.null(Invy)){
+    return(Invy)
+  }
+  else{
+    NeededMatrix<-y$getInverse()
+    Invy<-Solve(NeededMatrix)
+    y$setInverse(Invy)
+    return(NeededMatrix)
+  }
 }
